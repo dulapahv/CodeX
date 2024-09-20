68982b562114d8727f7bd335add07369002d20b6
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-09-21 00:52:17 +0700

    refactor: remove unused next-theme attributes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,14 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
       </body>
     </html>
   );
