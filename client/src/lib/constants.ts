a4a0561829eb7ee4fcfc40ce062dac21069e2d65
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-09-25 01:13:29 +0100

    fix: 404 error

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -4,6 +4,6 @@ export const BASE_SERVER_URL =
     ? "http://localhost:3001"
     : process.env.VERCEL_URL === "preview"
       ? "https://dev-occp-server.dulapahv.dev"
-      : "https://occp-server.dulapahv.dev";
+      : "ws://occp-server.dulapahv.dev";
 
 export const NAME_MAX_LENGTH = 255;
