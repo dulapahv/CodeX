dbe824dd1971c3a1e3db956b7c3584df1f11647b
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-11-13 03:11:08 +0000

    Update page.tsx

--- client/src/app/room/[roomId]/page.tsx
+++ client/src/app/room/[roomId]/page.tsx
@@ -43,9 +43,9 @@ export default function Room({ params }: RoomProps) {
   }, [params.roomId, socket]);
 
   useEffect(() => {
-    // if (!socket.connected) {
-    //   router.replace(`/?room=${params.roomId}`);
-    // }
+    if (!socket.connected) {
+      router.replace(`/?room=${params.roomId}`);
+    }
 
     // Request users and listen for updates
     socket.emit(RoomServiceMsg.GET_USERS);
