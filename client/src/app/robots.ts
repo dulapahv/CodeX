fceec8be8657eda2bc41561a06d119fdc80c1c52
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-11-16 15:52:31 +0000

    update

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: '/room/*', // Block all URLs that start with /room/
+        disallow: ['/room/*', '/oauth/*'], // Block all URLs that start with /room/ and /oauth/
       },
     ],
     host: SITE_URL,
