17e8d072f2c0923595a7f082c702efb56e0e0e39
Dulapah Vibulsanti <dulapah6@gmail.com>
2024-11-18 16:50:51 +0000

    update

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -60,7 +60,7 @@ export const metadata: Metadata = {
     site: '@dulapahv',
     images: [
       {
-        url: '/images/ogp.png',
+        url: '/images/cover.png',
         width: 1200,
         height: 630,
         alt: SITE_NAME,
@@ -76,7 +76,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/images/ogp.png',
+        url: '/images/cover.png',
         width: 1200,
         height: 630,
         alt: SITE_NAME,
--- client/src/components/about-dialog/index.tsx
+++ client/src/components/about-dialog/index.tsx
@@ -84,7 +84,7 @@ const AboutDialog = forwardRef<AboutDialogRef>((props, ref) => {
             aria-label="Kasca application preview"
           >
             <Image
-              src="/images/ogp.png"
+              src="/images/cover.png"
               alt="Kasca application interface preview"
               className="absolute rounded-md object-cover"
               fill
@@ -181,7 +181,7 @@ const AboutDialog = forwardRef<AboutDialogRef>((props, ref) => {
         <div className="w-full px-4" aria-label="Kasca application preview">
           <div className="relative aspect-[600/315] w-full max-w-full">
             <Image
-              src="/images/ogp.png"
+              src="/images/cover.png"
               alt="Kasca application interface preview"
               className="rounded-md object-cover"
               fill
