3059a3a706217ee3f13ca987b3fce610ddaaf39b
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-01-02 23:27:09 +0000

    test: refine playwright tests

--- client/tests/utils/setup.ts
+++ client/tests/utils/setup.ts
@@ -14,7 +14,7 @@ export async function createRoom(page: Page, name: string) {
   await expect(page.getByLabel('Redirecting')).toBeVisible();
 
   // Wait for room to be created and URL to change /room/:id
-  await page.waitForURL(/\/room\/.*/);
+  await page.waitForLoadState('networkidle');
 
   // Verify room joining
   const hasJoined = await hasJoinedRoom(page, name);
@@ -32,7 +32,7 @@ export async function joinRoom(page: Page, roomUrl: string, name: string) {
   await page.goto(roomUrl);
 
   // Wait for the room to be joined and URL to change /?room=:id
-  await page.waitForURL(/\/\?room=.*/);
+  await page.waitForLoadState('networkidle');
 
   // Fill name and join room
   await page.getByRole('textbox', { name: 'Name' }).fill(name);
