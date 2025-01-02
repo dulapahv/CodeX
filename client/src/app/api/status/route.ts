0ba4d118e230d49179663b7dffb18745f74b84f2
Dulapah Vibulsanti <dulapah6@gmail.com>
2025-01-02 20:54:10 +0000

    refactor: use edge runtime

--- client/src/app/api/status/route.ts
+++ client/src/app/api/status/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import type { BetterStackResponse } from '@/components/status/types';
 
+export const runtime = 'edge';
+
 export async function GET() {
   try {
     const response = await fetch(
